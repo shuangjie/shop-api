f64c3147c4de7370a2eaab0b7efe194bb856b8d1
DoubleJack <shuangjiezeng@gmail.com>
2024-04-26 20:13:51 +0800

    chore: 自定义验证器-手机号码

--- user-web/main.go
+++ user-web/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	ut "github.com/go-playground/universal-translator"
 
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	"go.uber.org/zap"
 
 	"shop-api/user-web/global"
 	"shop-api/user-web/initialize"
+	validator2 "shop-api/user-web/validator"
 )
 
 func main() {
@@ -16,6 +20,18 @@ func main() {
 	router := initialize.Routers()
 	if err := initialize.InitTrans("zh"); err != nil {
 		zap.S().Panic("初始化翻译器失败", err.Error())
+	}
+
+	// 注册验证器
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("mobile", validator2.ValidateMobile)
+		v.RegisterTranslation("mobile", global.Trans, func(ut ut.Translator) error {
+			return ut.Add("mobile", "{0} 非法的手机号码!", true) // see universal-translator for details
+		}, func(ut ut.Translator, fe validator.FieldError) string {
+			t, _ := ut.T("mobile", fe.Field())
+			return t
+		})
+
 	}
 
 	zap.S().Infof("user-web服务启动中..., 端口: %d", global.ServerConfig.Port)
