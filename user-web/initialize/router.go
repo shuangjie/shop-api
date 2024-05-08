277cfaa6169b887a8c0e9ccb0275fc0c49585550
DoubleJack <shuangjiezeng@gmail.com>
2024-05-08 18:14:39 +0800

    chore: 增加健康检查入口

--- user-web/initialize/router.go
+++ user-web/initialize/router.go
@@ -1,13 +1,22 @@
 package initialize
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+
 	"shop-api/user-web/middlewares"
 	"shop-api/user-web/router"
 )
 
 func Routers() *gin.Engine {
 	Router := gin.Default()
+	Router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    http.StatusOK,
+			"success": true,
+		})
+	})
 
 	Router.Use(middlewares.Cors())
 	ApiGroup := Router.Group("u/v1")
