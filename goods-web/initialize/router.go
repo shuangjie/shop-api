e8271904d47393c5d70c8aae7b1c73b6400b9543
DoubleJack <shuangjiezeng@gmail.com>
2024-05-22 12:32:27 +0800

    chore: Brand

--- goods-web/initialize/router.go
+++ goods-web/initialize/router.go
@@ -23,6 +23,7 @@ func Routers() *gin.Engine {
 	router.InitGoodsRouter(ApiGroup)
 	router.InitCategoryRouter(ApiGroup)
 	router.InitBannerRouter(ApiGroup)
+	router.InitBrandRouter(ApiGroup)
 
 	return Router
 }
