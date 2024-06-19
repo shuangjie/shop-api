893f44e5c25259a9ba051825e9edb2686fff82d6
DoubleJack <shuangjiezeng@gmail.com>
2024-06-19 22:06:23 +0800

    chore: 完成订单详情

--- order-web/api/order/order.go
+++ order-web/api/order/order.go
@@ -69,10 +69,64 @@ func List(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, reMap)
 }
 
-func New(context *gin.Context) {
+// Detail 订单详情
+func Detail(ctx *gin.Context) {
+	// 1. 获取用户的信息 for jwt
+	userId, _ := ctx.Get("userId")
+	claims, _ := ctx.Get("claims")
+	id := ctx.Param("id")
+	orderId, err := strconv.Atoi(id)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"msg": "参数错误",
+		})
+		return
+	}
 
+	// *：如果是管理员，不校验订单的用户id；如果是用户，校验订单的用户id
+	request := proto.OrderRequest{
+		Id: int32(orderId),
+	}
+	model := claims.(*models.CustomClaims)
+	if model.AuthorityId == 1 {
+		request.UserId = int32(userId.(uint))
+	}
+
+	// 2. 调用订单服务
+	rsp, err := global.OrderSrvClient.OrderDetail(ctx, &request) // mark: 这里 ctx 和 context.Background() 是一样的
+	if err != nil {
+		zap.S().Errorw("[Detail] 获取 [订单详情] 失败")
+		api.HandlerGrpcErrorToHttp(err, ctx)
+		return
+	}
+
+	// 3. 拼接返回结果
+	reMap := gin.H{
+		"id":       rsp.OrderInfo.Id,
+		"user":     rsp.OrderInfo.UserId,
+		"order_sn": rsp.OrderInfo.OrderSn,
+		"pay_type": rsp.OrderInfo.PayType,
+		"status":   rsp.OrderInfo.Status,
+		"total":    rsp.OrderInfo.Total,
+		"address":  rsp.OrderInfo.Address,
+		"name":     rsp.OrderInfo.Name,
+		"mobile":   rsp.OrderInfo.Mobile,
+		"post":     rsp.OrderInfo.Post,
+	}
+
+	list := make([]interface{}, 0)
+	for _, goods := range rsp.Goods {
+		tmpMap := gin.H{
+			"id":    goods.GoodsId,
+			"name":  goods.GoodsName,
+			"price": goods.GoodsPrice,
+			"image": goods.GoodsImage,
+			"nums":  goods.Nums,
+		}
+		list = append(list, tmpMap)
+	}
+	reMap["goods"] = list
+	ctx.JSON(http.StatusOK, reMap)
 }
 
-func Detail(context *gin.Context) {
-
-}
+func New(context *gin.Context) {}
